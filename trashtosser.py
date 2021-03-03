93177920e160dbe7c30fae6942cccfdc5475c0cd
ori <55019674+xxori@users.noreply.github.com>
2021-03-03 22:20:21 +1030

    Power and angle changing, moving calculation to ball class

--- trashtosser.py
+++ trashtosser.py
@@ -7,6 +7,8 @@ pygame.init()
 SIZE_X = 1000
 SIZE_Y = 500
 
+GRAVITY = 1
+
 
 screen = pygame.display.set_mode((SIZE_X,SIZE_Y))
 pygame.display.set_caption("Trash Tosser")
@@ -18,24 +20,43 @@ clock = pygame.time.Clock()
 class Ball(pygame.sprite.Sprite):
     def __init__(self, x, y, vx, vy):
         super().__init__()
+        # Position and velocity vectors
         self.pos = Vector2(x,y)
         self.vel = Vector2(vx,vy)
         # Random color for each ball
         self.color = (random.randint(0,255),random.randint(0,255),random.randint(0,128))
         self.paused = True
+        # Scalar for the ball so the launch power can be changed
+        self.scalar = 15
 
 
     def draw(self, surface):
+        # Draw circle and line showing velocity vector
         pygame.draw.circle(surface, self.color, self.pos, 20)
+        pygame.draw.line(screen, (255, 255, 255), self.pos, self.pos + (Vector2(self.vel.x,-self.vel.y) * 5), width=5)
 
     def update(self):
-        # Apply velocity
-        self.pos += Vector2(self.vel.x,-self.vel.y)
-        # Apply gravity
-        self.vel.y -= 1
+        # Only calculate if the ball is moving and not paused
+        if not self.paused and self.vel != Vector2(0,0):
+            # Apply velocity
+            self.pos += Vector2(self.vel.x,-self.vel.y)
+            # Apply gravity
+            self.vel.y -= GRAVITY
+
+            # If the ball is off screen
+            if self.pos.y >= 480 + self.vel.y:
+                # If the ball is going very slow then don't flip it and set the velocity to 0 instead
+                if abs(self.vel.x) < 5 and abs(self.vel.y) < 5 and self.pos.y > 450:
+                    self.vel = Vector2(0, 0)
+                    self.pos.y = 480
+                # Otherwise, flip the y velocity and times the velocity by 2/3 of it's current
+                else:
+                    self.vel.y *= -1
+                    self.vel.scale_to_length(self.vel.magnitude()*0.66)
 
 
 ball = Ball(20,400,10,10)
+ball.vel.scale_to_length(15)
 
 while not done:
     for event in pygame.event.get():
@@ -49,22 +70,33 @@ while not done:
                 # Reset ball pos
                 ball = Ball(20,400,10,10)
 
+    keys = pygame.key.get_pressed()
+    if ball.paused:
+        # Rotate with left and right arrows
+        if keys[pygame.K_LEFT]:
+            if ball.vel.angle_to(ball.pos) > 0:
+                ball.vel.rotate_ip(1)
+        if keys[pygame.K_RIGHT]:
+            if ball.vel.angle_to(ball.pos) < 80:
+                ball.vel.rotate_ip(-1)
+
+        # Change power with up and down
+        if keys[pygame.K_UP]:
+            if ball.scalar < 20:
+                ball.scalar += 0.1
+                ball.vel.scale_to_length(ball.scalar)
+        if keys[pygame.K_DOWN]:
+            if ball.scalar > 10:
+                ball.scalar -= 0.1
+                ball.vel.scale_to_length(ball.scalar)
+
+    # Update and draw ball with blue background
+    ball.update()
+
     screen.fill((0,0,255))
     ball.draw(screen)
 
-    # Update ball position if it's in motion and don't calculate if it has no velocity
-    if not ball.paused and ball.vel != Vector2(0,0):
-        ball.update()
 
-    # If the ball is off the screen then half its velocity and flip the y value
-    if ball.pos.y >= 480 and ball.pos.magnitude() > 0:
-        ball.vel.y *= -1
-        ball.vel.y /= 2
-        ball.vel.x /= 2
-
-    # Set the ball's velocity to to zero if it's really small to stop dividing infinitely
-    if ball.vel.x < 1 and ball.vel.y < 1:
-        ball.vel = Vector2(0,0)
 
     # Flip display and limit to 60 fps
     pygame.display.update()
